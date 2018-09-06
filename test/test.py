f3cee5def72f3cfe91e7f7ba65020ef72e4c12b1
Balaji Natrajan <balaji.natrajan@microsemi.com>
2018-09-06 09:32:42 -0500

    Added test to verify successful dictionary generation of all csdl files

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 SPEC_INDEX_SCHEMA_FILENAME = 0
 SPEC_INDEX_SCHEMA_ENTITY = 1
@@ -40,13 +41,35 @@ MAJOR_SCHEMA_DICTIONARY_LIST = [('Drive_v1.xml',
                                 ]
 
 if __name__ == '__main__':
+    # go thru every csdl and attempt creating a dictionary
+    skip_list = ['AttributeRegistry_v1.xml'] # TODO: find and fix why these are failing
+    schema_test_dir = 'test/schema'
+    for filename in os.listdir(schema_test_dir + '/metadata'):
+        if filename not in skip_list:
+            # strip out the _v1.xml
+            m = re.compile('(.*)_v1.xml').match(filename)
+            if m:
+                entity = m.group(1) + '.' + m.group(1)
+
+            dict_cmd = 'python rde_schema_dictionary_gen.py local ' \
+                       '--csdlSchemaDirectories ' + schema_test_dir + '/metadata ' \
+                       '--jsonSchemaDirectories ' + schema_test_dir + '/json-schema ' \
+                       '--schemaFilename ' + filename + \
+                       ' --entity ' + entity + \
+                       ' --outputFile ' + 'tmp_dict.bin'
+            print(dict_cmd)
+            error_code = os.system(dict_cmd)
+            if error_code != 0:
+                exit(error_code)
 
     # Generate the annotation dictionary
     print('Generating annotation dictionary...')
-    os.system('python rde_schema_dictionary_gen.py annotation '
-              '--csdlSchemaDirectories test/schema/metadata '
-              '--jsonSchemaDirectories test/schema/json-schema '
-              '--outputFile annotation.bin')
+    error_code = os.system('python rde_schema_dictionary_gen.py annotation '
+                           '--csdlSchemaDirectories test/schema/metadata '
+                           '--jsonSchemaDirectories test/schema/json-schema '
+                           '--outputFile annotation.bin')
+    if error_code != 0:
+        exit(error_code)
 
     # Generate the major schema dictionaries
     for major_schema in MAJOR_SCHEMA_DICTIONARY_LIST:
@@ -63,7 +86,9 @@ if __name__ == '__main__':
                    major_schema[SPEC_INDEX_PROFILE] + \
                    ' --outputFile ' + major_schema[SPEC_INDEX_DICTIONARY_FILENAME]
         print(dict_cmd)
-        os.system(dict_cmd)
+        error_code = os.system(dict_cmd)
+        if error_code != 0:
+            exit(error_code)
 
         # Run the encode/decode
         encode_cmd = 'python pldm_bej_encoder_decoder.py encode '\
@@ -73,7 +98,9 @@ if __name__ == '__main__':
                      ' --bejOutputFile ' + major_schema[SPEC_INDEX_BEJ_ENCODED_FILE] + \
                      ' --pdrMapFile pdr.txt'
         print(encode_cmd)
-        os.system(encode_cmd)
+        error_code = os.system(encode_cmd)
+        if error_code != 0:
+            exit(error_code)
 
         decode_cmd = 'python pldm_bej_encoder_decoder.py decode '\
                      '--schemaDictionary ' + major_schema[SPEC_INDEX_DICTIONARY_FILENAME] + \
@@ -87,4 +114,5 @@ if __name__ == '__main__':
         print('Decoded JSON:')
         print(json.dumps(json.loads(decode_file), indent=3))
         assert(json.loads(decode_file) == json.load(open('test/'+major_schema[SPEC_INDEX_JSONFILE_TO_ENCODE])))
+
     exit(code=0)
