3236e93a624a3777f092b3a5b71c9cad8d547ee4
Patrick Kilgore <Patrick.Kilgore@microchip.com>
2025-10-27 16:14:20 -0500

    Add special character escapes for bejString encoding (#76)
    Updated encoding of bejString tuple values to properly escape special characters
    per the requirements of DSP0218 v1.2.0 Table 16.

--- rdebej/encode.py
+++ rdebej/encode.py
@@ -133,7 +133,18 @@ def bej_pack_sfl(stream, seq_num, format, length, format_flags):
 
 
 def bej_pack_sflv_string(stream, seq_num, str, format_flags):
-    str = str.replace('"', '\\"')
+    escape_sequences = [
+        ('\\', '\\\\'),
+        ('"',  '\\"'),
+        ('/',  '\\/'),
+        ('\b', '\\b'),
+        ('\f', '\\f'),
+        ('\n', '\\n'),
+        ('\r', '\\r')
+    ]
+    for old, new in escape_sequences:
+        str = str.replace(old, new)
+
     num_bytes_packed = bej_pack_sfl(stream, seq_num, BEJ_FORMAT_STRING, len(str) + 1, format_flags)
 
     # pack str
